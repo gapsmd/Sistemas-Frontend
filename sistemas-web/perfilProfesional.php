efb95e326e71f455fd9c3c511d66cb354fcccbe5
gapsmd <l18020781@veracruz.tecnm.mx>
2022-05-06 15:48:14 -0500

    Bases

---
+++ sistemas-web/perfilProfesional.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'includes/header.html';
+require_once 'perfilProfesional-vista.html';
+require_once 'includes/footer.html';
+?>
---
+++ sistemas-web/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    'includes/header.html',
+    'includes/footer.html',
+    'index.html',
+    'perfilProfesional-vista.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'font-Montserrat': ['Montserrat', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
